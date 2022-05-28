62610bceeec0ea15affa024509000f328d4678a7
Documenter.jl <documenter@juliadocs.github.io>
2022-05-28 13:55:58 +0000

    build based on a3f3f6e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Spellcircles","category":"page"},{"location":"#Spellcircles","page":"Home","title":"Spellcircles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Spellcircles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Spellcircles]","category":"page"}]
+}
