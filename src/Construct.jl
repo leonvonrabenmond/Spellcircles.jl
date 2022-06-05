a5623b65a29a5823b30016d09fe9e71261768941
Leon von Rabenmond <leon@rabenmond.org>
2022-06-05 21:13:35 +0200

    Reordered Constructs.jl

--- src/Construct.jl
+++ src/Construct.jl
@@ -1,31 +1,4 @@
-"""
-polygonpt(n,r)
-
-Compute all points of any regular `n`-polygon with radius `r`.
-
-The first point is located at `(0,r)` and all further points are added clockwise.
-
-# Arguements
-- `orientation::Number`: Rotates the polygon clockwise by `θ` degrees.
-- `x::Number`: Moves polygon by `x`.
-- `y::Number`: Moves polygon by `y`.
-
-# Examples
-```julia-repl
-julia> polygonpt(3,1)
-3-element Vector{Tuple{Float64, Float64}}:
-  (0.0, 1.0)
-  (0.87, -0.5)
-  (-0.87, -0.5)
-```
-"""
-function polygonpt(n::Number,r::Number;orientation::Number=0,x::Number=0,y::Number=0)
-    complexvectors = [round(r*ℯ^(deg2rad(90-i*360/n -orientation) * im), digits = 2) for i ∈ 0:(n-1)];
-	polygonptpt = [[real(complexvectors[i])+x, imag(complexvectors[i])+y] for i ∈ 1:length(complexvectors)]
-end
-
-
-
+# Universal
 """
 blank()
 
@@ -70,6 +43,33 @@ function construct(;name::String="Spellcircle", size::Int=100, custom::Bool=fals
 	close(spellcircleconstruct)
 end
 
+# Polygons
+"""
+polygonpt(n,r)
+
+Compute all points of any regular `n`-polygon with radius `r`.
+
+The first point is located at `(0,r)` and all further points are added clockwise.
+
+# Arguements
+- `orientation::Number`: Rotates the polygon clockwise by `θ` degrees.
+- `x::Number`: Moves polygon by `x`.
+- `y::Number`: Moves polygon by `y`.
+
+# Examples
+```julia-repl
+julia> polygonpt(3,1)
+3-element Vector{Tuple{Float64, Float64}}:
+  (0.0, 1.0)
+  (0.87, -0.5)
+  (-0.87, -0.5)
+```
+"""
+function polygonpt(n::Number,r::Number;orientation::Number=0,x::Number=0,y::Number=0)
+    complexvectors = [round(r*ℯ^(deg2rad(90-i*360/n -orientation) * im), digits = 2) for i ∈ 0:(n-1)];
+	polygonptpt = [[real(complexvectors[i])+x, imag(complexvectors[i])+y] for i ∈ 1:length(complexvectors)]
+end
+
 
 """
 polygon(n,r)
@@ -96,28 +96,6 @@ function polygon(n::Number,r::Number;orientation::Number=0,x::Number=0,y::Number
 	push!(rawspellcircle, polygon)
 	return nothing
 end
-
-
-"""
-circle(r)
-
-Constructs a circle with radius `r`.
-
-# Arguments
-- `x::Number`: Moves polygon by `x`.
-- `y::Number`: Moves polygon by `y`.
-- `stroke::String`: Sets the colour of the lines.
-- `width::Number`: Sets the width of the lines.
-- `fill::String`: Sets background colour of the polygon.
-"""
-function circle(r::Number; x::Number=0,y::Number=0, stroke::String="black", width::Number=1, fill::String="none")
-	circle = """	<circle cx="$x" cy="$(-y)" r="$r" stroke="$stroke" stroke-width="$width" fill="$fill" />
-	      """
-	push!(rawspellcircle, circle)
-	return nothing
-end
-
-
 
 
 function polygrampt(n::Number,r::Number;orientation::Number=0,x::Number=0,y::Number=0,edges::Int=1)
@@ -171,5 +149,25 @@ function polygram(n::Number, r::Number; orientation::Number=0, x::Number=0, y::N
 	       polygram = """   <line x1="$(pt[i][1][1])" y1="$(-pt[i][1][2])" x2="$(pt[i][2][1])" y2="$(-pt[i][2][2])" fill="$fill" stroke="$stroke" stroke-width="$width" /> """
 	       push!(rawspellcircle, polygram)
     end
+	return nothing
+end
+
+# Circles
+"""
+circle(r)
+
+Constructs a circle with radius `r`.
+
+# Arguments
+- `x::Number`: Moves polygon by `x`.
+- `y::Number`: Moves polygon by `y`.
+- `stroke::String`: Sets the colour of the lines.
+- `width::Number`: Sets the width of the lines.
+- `fill::String`: Sets background colour of the polygon.
+"""
+function circle(r::Number; x::Number=0,y::Number=0, stroke::String="black", width::Number=1, fill::String="none")
+	circle = """	<circle cx="$x" cy="$(-y)" r="$r" stroke="$stroke" stroke-width="$width" fill="$fill" />
+	      """
+	push!(rawspellcircle, circle)
 	return nothing
 end
